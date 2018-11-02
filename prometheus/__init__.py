d3b30d9c12422585892e78ef03fbfa8b12bafc5b
Steffen Ohrendorf <steffen.ohrendorf@traveltainment.de>
2018-11-02 09:09:01 +0100

    Expose information about tracked metrics and gc time

--- prometheus/__init__.py
+++ prometheus/__init__.py
@@ -3,8 +3,8 @@ import re
 import time
 from threading import RLock
 from typing import Union, Dict, Tuple, FrozenSet
-from prometheus_client import Counter
-from prometheus_client.core import _LabelWrapper
+from prometheus_client import Counter, Summary
+from prometheus_client.core import _LabelWrapper, Gauge
 
 
 class _CounterGC:
@@ -15,27 +15,33 @@ class _CounterGC:
         self._counters_ttl = {}  # type: Dict[ Tuple[str, FrozenSet[Tuple[str, str]]], Tuple[_LabelWrapper, float] ]
         self._counters = {}  # type: Dict[ str, Tuple[_LabelWrapper, float] ]
         self._lock = RLock()
+        self._active_metrics = Gauge(name='scraper_active_metrics',
+                                     documentation='Number of non-stale (tracked) metrics')
+        self._gc_duration = Summary('scraper_gc_duration_seconds',
+                                    documentation='Duration of stale metric removal')
 
     def gc(self):
-        now = time.time()
-        with self._lock:
-            cleaned = {}  # type: Dict[ Tuple[str, FrozenSet[Tuple[str, str]]], Tuple[_LabelWrapper, float] ]
-            dropped = 0
-            for key, counter_ttl in self._counters_ttl.items():
-                counter = counter_ttl[0]
-                last_used = counter_ttl[1]
-                if last_used + self._ttl < now:
-                    labels = dict(key[1])
-                    values = [labels[lbl] for lbl in counter._labelnames]
-                    counter.remove(*values)
-                    counter.labels(**labels)
-                    dropped += 1
-                else:
-                    cleaned[key] = counter_ttl
-            self._counters_ttl = cleaned
+        with self._gc_duration.time():
+            now = time.time()
+            with self._lock:
+                cleaned = {}  # type: Dict[ Tuple[str, FrozenSet[Tuple[str, str]]], Tuple[_LabelWrapper, float] ]
+                dropped = 0
+                for key, counter_ttl in self._counters_ttl.items():
+                    counter = counter_ttl[0]
+                    last_used = counter_ttl[1]
+                    if last_used + self._ttl < now:
+                        labels = dict(key[1])
+                        values = [labels[lbl] for lbl in counter._labelnames]
+                        counter.remove(*values)
+                        counter.labels(**labels)
+                        dropped += 1
+                    else:
+                        cleaned[key] = counter_ttl
+                self._counters_ttl = cleaned
 
-        if dropped > 0:
-            logging.getLogger().info('Dropped {} metric(s) due to exceeding TTL'.format(dropped))
+            self._active_metrics.set(len(self._counters_ttl))
+            if dropped > 0:
+                logging.getLogger().info('Dropped {} metric(s) due to exceeding TTL'.format(dropped))
 
     def inc(self, full_name: str, documentation: str, labels: Dict[str, str], amount: float):
         with self._lock:
@@ -45,6 +51,8 @@ class _CounterGC:
                 self._counters[full_name] = counter
 
             key = (full_name, frozenset(labels.items()))
+            if key not in self._counters_ttl:
+                self._active_metrics.inc()
             self._counters_ttl[key] = (counter, time.time())
             counter.labels(**labels).inc(amount)
 
