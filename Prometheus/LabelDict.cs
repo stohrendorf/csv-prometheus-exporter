89d3f6370595bde3c1452701b1560989a46e82c1
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2019-02-24 19:31:31 +0100

    Add tests for LabelDict

--- Prometheus/LabelDict.cs
+++ Prometheus/LabelDict.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
+using NUnit.Framework;
 
 namespace csv_prometheus_exporter.Prometheus
 {
@@ -9,18 +11,20 @@ namespace csv_prometheus_exporter.Prometheus
     /// </summary>
     public sealed class LabelDict
     {
-        private readonly string _environment;
+        internal readonly string Environment;
         private readonly IList<KeyValuePair<string, string>> _labels = new List<KeyValuePair<string, string>>();
 
         public LabelDict([NotNull] LabelDict other)
         {
-            _environment = other._environment;
+            Environment = other.Environment;
             _labels = new List<KeyValuePair<string, string>>(other._labels);
         }
 
         public LabelDict([NotNull] string environment)
         {
-            _environment = environment;
+            if (string.IsNullOrEmpty(environment))
+                throw new ArgumentNullException(nameof(environment), "Environment must contain a value");
+            Environment = environment;
         }
 
         public void Set(string key, string value)
@@ -43,7 +47,7 @@ namespace csv_prometheus_exporter.Prometheus
 
         private bool Equals(LabelDict other)
         {
-            if (_environment != other._environment)
+            if (Environment != other.Environment)
                 return false;
 
             if (_labels.Count != other._labels.Count)
@@ -65,7 +69,7 @@ namespace csv_prometheus_exporter.Prometheus
 
         public override int GetHashCode()
         {
-            var result = _environment.GetHashCode();
+            var result = Environment.GetHashCode();
             foreach (var (key, value) in _labels)
                 result = result * 31 + key.GetHashCode() * 17 + value.GetHashCode();
 
@@ -77,9 +81,9 @@ namespace csv_prometheus_exporter.Prometheus
         /// </summary>
         /// <param name="le">Optional "le" key for histogram buckets.</param>
         /// <returns></returns>
-        public string ToString(string le)
+        public string ToString([CanBeNull] string le)
         {
-            var result = $"environment={Quote(_environment)}";
+            var result = $"environment={Quote(Environment)}";
             if (!string.IsNullOrEmpty(le))
                 result += ",le=" + Quote(le);
 
@@ -100,5 +104,87 @@ namespace csv_prometheus_exporter.Prometheus
         }
 
         public int Count => _labels.Count;
+    }
+
+    [TestFixture]
+    public class LabelDictTest
+    {
+        [Test]
+        public void TestConstruction()
+        {
+            Assert.That(() => new LabelDict((string) null), Throws.TypeOf<ArgumentNullException>());
+            Assert.That(() => new LabelDict(""), Throws.TypeOf<ArgumentNullException>());
+
+            var d = new LabelDict("foo");
+            Assert.That(d.Environment, Is.EqualTo("foo"));
+            Assert.That(d.Count, Is.EqualTo(0));
+
+            var d2 = new LabelDict(d);
+            Assert.That(d2.Environment, Is.EqualTo("foo"));
+            Assert.That(d2.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestEquality()
+        {
+            var d1 = new LabelDict("foo");
+            var d2 = new LabelDict(d1);
+            var d3 = new LabelDict("foo");
+            var d4 = new LabelDict("bar");
+            var d5 = new LabelDict("foo");
+            d5.Set("bar", "baz");
+            var d6 = new LabelDict("foo");
+            d6.Set("bar", "baz2");
+
+            Assert.That(d1, Is.EqualTo(d1));
+            Assert.That(d1, Is.Not.EqualTo(null));
+            Assert.That(d1, Is.EqualTo(d2));
+            Assert.That(d1, Is.EqualTo(d3));
+            Assert.That(d1, Is.EqualTo(d2));
+            Assert.That(d1, Is.Not.EqualTo(d4));
+            Assert.That(d1, Is.Not.EqualTo(d5));
+            Assert.That(d5, Is.Not.EqualTo(d6));
+        }
+
+        [Test]
+        public void TestSet()
+        {
+            var d = new LabelDict("foo");
+
+            d.Set("baz", "baz");
+            Assert.That(d.Count, Is.EqualTo(1));
+            Assert.That(d.Get("baz"), Is.EqualTo("baz"));
+
+            d.Set("baz", "baz2");
+            Assert.That(d.Count, Is.EqualTo(1));
+            Assert.That(d.Get("baz"), Is.EqualTo("baz2"));
+
+            d.Set("foo", "bar");
+            Assert.That(d.Count, Is.EqualTo(2));
+            Assert.That(d.Get("baz"), Is.EqualTo("baz2"));
+            Assert.That(d.Get("foo"), Is.EqualTo("bar"));
+        }
+
+        [Test]
+        public void TestToString()
+        {
+            var d = new LabelDict("foo");
+            Assert.That(d.ToString(null), Is.EqualTo("environment=\"foo\""));
+            Assert.That(d.ToString(""), Is.EqualTo("environment=\"foo\""));
+            Assert.That(d.ToString("123"), Is.EqualTo("environment=\"foo\",le=\"123\""));
+
+            d = new LabelDict("foo\nbar");
+            Assert.That(d.ToString(null), Is.EqualTo("environment=\"foo\\nbar\""));
+
+            d = new LabelDict("foo\"bar");
+            Assert.That(d.ToString(null), Is.EqualTo("environment=\"foo\\\"bar\""));
+
+            d = new LabelDict("foo\\bar");
+            Assert.That(d.ToString(null), Is.EqualTo("environment=\"foo\\\\bar\""));
+
+            d = new LabelDict("foo");
+            d.Set("a", "b");
+            Assert.That(d.ToString(null), Is.EqualTo("environment=\"foo\",a=\"b\""));
+        }
     }
 }
